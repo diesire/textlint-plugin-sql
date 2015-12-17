d59cebee6aaa11f898779f65e5fb3f896300ef21
Pablo <diesire@gmail.com>
2015-12-17 13:52:34 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# textlint-plugin-sql
+
+text support for [textlint](https://github.com/textlint/textlint "textlint").
+
+## Installation
+
+    npm install textlint-plugin-sql
+
+## Usage
+
+Manually add sql plugin to do following:
+
+```
+{
+    "plugins": [
+        "sql"
+    ]
+}
+```
+
+## Tests
+
+    npm test
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## License
+
+MIT
---
+++ src/TextProcessor.js
@@ -0,0 +1,28 @@
+// LICENSE : MIT
+"use strict";
+import {parse} from "txt-to-ast";
+export default class TextProcessor {
+    constructor(config) {
+        this.config = config;
+    }
+
+    static availableExtensions() {
+        return [
+            ".sql"
+        ];
+    }
+
+    processor(ext) {
+        return {
+            preProcess(text, filePath) {
+                return parse(text);
+            },
+            postProcess(messages, filePath) {
+                return {
+                    messages,
+                    filePath: filePath ? filePath : "<sql>"
+                };
+            }
+        };
+    }
+}
